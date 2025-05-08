4a06c6f66d7bebd77c9afb96f248f18fcde51f31
Jayani adikari <mjmadikari@students.nsbm.ac.lk>
2025-05-08 13:47:28 +0530

    Singleton utility class (TestApp) for WebDriver management

--- gui-test/src/main/java/com/guru99/demo/RegisterSuccessPage.java
+++ gui-test/src/main/java/com/guru99/demo/RegisterSuccessPage.java
@@ -1,4 +1,15 @@
 package com.guru99.demo;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import utils.TestApp;
+
 public class RegisterSuccessPage {
+
+    WebDriver driver= TestApp.getInstance().getDriver();
+
+    public String getSalutationMessage( ) {
+        TestApp.getInstance().waitUntilNextElementAppears(  By.xpath("//*[contains(text(),\"Dear\")]"), 10);
+        return driver.findElement(By.xpath("//*[contains(text(),\"Dear\")]")).getText();
+    }
 }
